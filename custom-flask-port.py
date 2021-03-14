653d54ed4abcfe7adfc4286f1d73d1e81ba2d563
glitch <saniltison@gmail.com>
2021-03-14 00:14:22 -0500

    The feature/test was SAT
    - The following was done:
        - entered custom setting to the app.run function
            - host
                - 192.168.1.81
            - port
                - 1146
    
    - to aid in debugging the app the debug attribute was set to TRUE

--- custom-flask-port.py
+++ custom-flask-port.py
@@ -1,2 +1,14 @@
-import flask
+from flask import Flask
 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "This is a simple Python Flask App"
+
+
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run(host='192.168.1.81', port=1146)
